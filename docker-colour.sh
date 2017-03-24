bb17de86d8526f0eb8ce4a5b2a4784ea8d2adf6c
Adrian Moisey <adrian@changeover.za.net>
2017-03-24 09:50:59 +0200

    Colourize docker output

---
+++ docker-colour.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ack --flush --passthru --color --color-match=green "^Step\ \d+/\d+ : .*$" \
+  | ack --flush --passthru --color --color-match=yellow "\ --->.*" \
+  | ack --flush --passthru --color --color-match=cyan "Removing intermediate container.*"
